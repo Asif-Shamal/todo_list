6ea08d6c7c0350117b433b74ed6768fcdc6209a2
Asif Shamal <asifshamal51@outlook.com>
2024-12-17 13:15:34 +0430

    setup shared preference for theme

--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDarkMode = false;
 
   bool get isDarkMode => _isDarkMode;
 
-  void toggleTheme() {
+  ThemeProvider() {
+    _loadThemePreference();
+  }
+
+  // Load the theme  from Shared Preference
+  Future<void> _loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    notifyListeners();
+  }
+
+  // Toggle & save theme to the preference
+  Future<void> toggleTheme() async {
     _isDarkMode = !_isDarkMode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', _isDarkMode);
     notifyListeners();
   }
 }
