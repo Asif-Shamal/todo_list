a06cb299b42d278a7b3d156a3f13223f3a545ecc
Asif Shamal <asifshamal51@outlook.com>
2024-12-17 15:59:24 +0430

    changes done by ide after adding url_luncher dependency

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/pages/about_us.dart';
 import 'package:todo_list/pages/home_page.dart';
 import 'package:todo_list/pages/settings.dart';
 
